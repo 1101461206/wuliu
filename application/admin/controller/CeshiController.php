0abf2ace21dc08cc6369f79246384809e30044a2
1101461206 <1101461206@qq.com>
2019-08-06 13:44:03 +0800

    ceshi

--- application/admin/controller/CeshiController.php
+++ application/admin/controller/CeshiController.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace app\admin\controller;
+
 use app\api\model\ApiModel;
 use think\Controller;
 use app\api\model\ApiModel as api;
+use think\Db;
 
 
 class CeshiController extends Controller
 {
-   // protected $merchant_id;
+    // protected $merchant_id;
 //    public function initialize()
 //    {
 //        $userinfo=Session::get('admin_info');
@@ -18,15 +21,19 @@ class CeshiController extends Controller
 //        $this->assign('admin_name',$userinfo['user_name']);
 //    }
 
-    public function indexAction(){
-        $ims_url=array('img_url'=>"https://xiao-1256168726.cos.ap-chengdu.myqcloud.com/1.png");
-        $api=new ApiModel();
-        $info=$api->discern('GeneralBasicOCR',$ims_url);
-
+    public function indexAction()
+    {
+        $ims_url = array('img_url' => "https://xiao-1256168726.cos.ap-chengdu.myqcloud.com/1.png");
+        $api = new ApiModel();
+        $info = $api->discern('GeneralBasicOCR', $ims_url);
+        $data=array(
+            'locatoin'=>$info;
+        );
+        $in = Db::name('xiao_form')->insertGetId($data);
         echo "<pre>";
-	 var_dump($info,true);
-	echo "<pre>";
+        var_dump($info, true);
+        echo "<pre>";
     }
-      //  return $this->fetch('order/index');
-    }
+    //  return $this->fetch('order/index');
+}
 
