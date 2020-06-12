91248bbf650f53ef10c07205ebd49130de0b629c
1101461206 <1101461206@qq.com>
2020-06-12 15:06:46 +0800

    参数配置

--- application/api/config/app.php
+++ application/api/config/app.php
@@ -7,4 +7,16 @@ return [
     'wx_token'=>'acjbzhf0izjck2w1i114nnnq30hnfg40',
     'wx_appid'=>'wx4c3829d268004a40',
     'wx_appsecret'=>'3b5a7973d54befe6c650f67a1c81ec57',
+
+    // +----------------------------------------------------------------------
+    // | 企业微信设置
+    // +----------------------------------------------------------------------
+    'wx_qy_token'=>'acjbzhf0izjck2w1i114nnnq30hnfg40',
+    'wx_qy_appid'=>'wx4c3829d268004a40',
+    'wx_qy_aeskey'=>'Xyemma4Bed6IuMm1rJ7HyXraKsTYSKjVKpWBN3mhUzD',
+    'wx_qy_secret'=>'Nu9b60Xi0F7k2EjG7krVH4QO8LvIih7AODgpBTX3xxA',
+    'wx_qy_id'=>'wx5788897e992e508d',
+
+
+
 ];
